7157023f043ce80309bfa9b28644957a6ad86879
TigerLilly64 <info@oberon-oss.eu>
2025-08-29 11:08:29 +0200

    Moved settings init to static{}. Fixed generation of invalid 1st character: in case a valid char should be generated, it was selecting character from VALID_FIRST_CHAR instead of VALID_SECOND_CHAR

--- base/src/main/java/eu/oberon/oss/util/test/PostCodeGenerator.java
+++ base/src/main/java/eu/oberon/oss/util/test/PostCodeGenerator.java
@@ -14,31 +14,38 @@ public class PostCodeGenerator extends AbstractBaseGenerator<String> {
     private final TextGenerator generator;
     private final NLPostalCodeChecker checker = new NLPostalCodeChecker();
 
-    private final TextGeneratorSettings validFirstChar;
-    private final TextGeneratorSettings validSecondChar;
-    private final TextGeneratorSettings invalidChar;
+    private static final TextGeneratorSettings VALID_FIRST_CHAR;
+    private static final TextGeneratorSettings VALID_SECOND_CHAR;
+    private static final TextGeneratorSettings INVALID_CHAR;
+
+    static {
+        VALID_FIRST_CHAR = TextGenerator.getBuilder()
+                .setMaxLength(1)
+                .addCharacterList(ALPHABETICAL)
+                .build();
+        VALID_SECOND_CHAR = TextGenerator.getBuilder()
+                .setMaxLength(1)
+                .addCharacterList(ALPHABETICAL.replaceAll("[ADS]", ""))
+                .build();
+        INVALID_CHAR = TextGenerator.getBuilder()
+                .setMaxLength(1)
+                .addCharacterList(" 0123456789!@#$%^&*()_+=-{}:<>/?|\\[];',.").addCharacterList(DIGITS)
+                .build();
+    }
 
     public PostCodeGenerator() {
         super(true);
         generator = new TextGenerator(getRandomGenerator());
-        validFirstChar = TextGenerator.getBuilder().setMaxLength(1).addCharacterList(ALPHABETICAL).build();
-        validSecondChar =
-                TextGenerator.getBuilder().setMaxLength(1).addCharacterList(ALPHABETICAL.replaceAll("[ADS]", "")).build();
-
-        invalidChar = TextGenerator.getBuilder()
-                .setMaxLength(1)
-                .addCharacterList(" !@#$%^&*()_+=-{}:<>/?|\\[];',.").addCharacterList(DIGITS)
-                .build();
     }
 
     @Override
     public String generateSingleValue(boolean generateValidItem) {
-        String postCode = null;
+        String postCode;
         if (generateValidItem) {
             postCode = String.format("%04d %S%S",
                     getRandomGenerator().nextInt(1000, 9999),
-                    generator.createText(validFirstChar),
-                    generator.createText(validSecondChar)
+                    generator.createText(VALID_FIRST_CHAR),
+                    generator.createText(VALID_SECOND_CHAR)
             );
         } else {
             do {
@@ -46,11 +53,11 @@ public class PostCodeGenerator extends AbstractBaseGenerator<String> {
                         ? getRandomGenerator().nextInt(1000, 9999)
                         : getRandomGenerator().nextInt(0, 999);
                 String s1 = getRandomGenerator().nextBoolean()
-                        ? generator.createText(invalidChar)
-                        : generator.createText(validSecondChar);
+                        ? generator.createText(INVALID_CHAR)
+                        : generator.createText(VALID_FIRST_CHAR);
                 String s2 = getRandomGenerator().nextBoolean()
-                        ? generator.createText(invalidChar)
-                        : generator.createText(validSecondChar);
+                        ? generator.createText(INVALID_CHAR)
+                        : generator.createText(VALID_SECOND_CHAR);
                 postCode = String.format("%04d %S%S", number, s1, s2);
 
             } while (checker.isValid(postCode));
