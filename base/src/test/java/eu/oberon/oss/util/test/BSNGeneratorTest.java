5abda9c1933c5947bc573ffca48ab274c4452ea2
TigerLilly64 <info@oberon-oss.eu>
2025-08-30 16:57:50 +0200

    Added BSN generator

---
+++ base/src/test/java/eu/oberon/oss/util/test/BSNGeneratorTest.java
@@ -0,0 +1,33 @@
+package eu.oberon.oss.util.test;
+
+import eu.oberon.oss.tools.validators.proofs.AvailableElevenProofs;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class BSNGeneratorTest {
+
+    static Stream<Arguments> bsnNumberList() {
+        List<Arguments> list = new ArrayList<>();
+
+        for (String bsn : new BSNGenerator().generateValueList(100, true)) {
+            list.add(Arguments.of(true, bsn));
+        }
+        for (String bsn : new BSNGenerator().generateValueList(100, false)) {
+            list.add(Arguments.of(false, bsn));
+        }
+        return list.stream();
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void bsnNumberList(boolean valid, String bsn) {
+        assertEquals(valid, AvailableElevenProofs.BSN.isValid(bsn));
+    }
+}
